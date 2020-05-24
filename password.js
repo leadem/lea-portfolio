7e15e0aa1bafb541e7e40cf8c8af1b8276c59db3
Léa De Miras <lea.de.miras@gmail.com>
2020-05-24 20:16:21 +0100

    first commit

---
+++ password.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+            function checkPswd() {
+                var confirmPassword = "LeaDM#2020";
+                var password = document.getElementById("pswd").value;
+                if (password == confirmPassword) {
+                    window.location="file:///Users/futoncompany/Downloads/MY%20FOLDER/Website/website/index.html";
+                }
+                else{
+                    alert("Passwords do not match.");
+                }
+            }
+        </script>
